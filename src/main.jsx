c914aaf1399c4cc4ab00978a32ff118fa5389d47
Moon Myoungsun <moonmyoungsun@gmail.com>
2025-12-13 19:53:42 +0900

    git commit -m "wip"

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// PHP 랜딩에 있는 mount 지점
+const el = document.getElementById("order-app");
+
+// 안전장치
+if (el) {
+  const mcode = el.dataset.mcode;
+
+  ReactDOM.createRoot(el).render(
+    <React.StrictMode>
+      <App mcode={mcode} />
+    </React.StrictMode>
+  );
+}
