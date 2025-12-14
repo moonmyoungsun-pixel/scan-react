f8a48ef89d2609dc640ec14fd0a27dacc4be66d3
Moon Myoungsun <moonmyoungsun@gmail.com>
2025-12-14 11:59:24 +0900

    git commit -m "feat: complete Orders with status toggle and server persistence"

--- order/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import App from './App';
-
-const el = document.getElementById('order-app');
-const mcode = el?.dataset?.mcode;
-
-if (el) {
-  createRoot(el).render(<App mcode={mcode} />);
-}
